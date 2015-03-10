3736388b776717b6bf16c6be3be85399e42ab849
George Raber <graber@gmail.com>
2015-03-10 13:40:56 -0500

    Update to dependent on Layer Selector

--- plugins/flood_slr/main.js
+++ plugins/flood_slr/main.js
@@ -1,111 +1,20 @@
-﻿// "layer_selector" plugin, main module
-
-// Plugins should load their own versions of any libraries used even if those libraries are also used 
-// by the GeositeFramework, in case a future framework version uses a different library version. 
-
-require({
-    // Specify library locations.
-    // The calls to location.pathname.replace() below prepend the app's root path to the specified library location. 
-    // Otherwise, since Dojo is loaded from a CDN, it will prepend the CDN server path and fail, as described in
-    // https://dojotoolkit.org/documentation/tutorials/1.7/cdn
-    packages: [
-        {
-            name: "jquery",
-            location: "//ajax.googleapis.com/ajax/libs/jquery/1.9.0",
-            main: "jquery.min"
-        },
-        {
-            name: "underscore",
-            location: "//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.4.4",
-            main: "underscore-min"
-        },
-        {
-            name: "extjs",
-            location: "//cdn.sencha.io/ext-4.1.1-gpl",
-            main: "ext-all"
-        },
-        {
-            name: "jquery.placeholder",
-            location: location.pathname.replace(/\/[^/]+$/, "") + "plugins/layer_selector/lib",
-            main: "jquery.placeholder.amd.min"
-        },
-        {
-            name: "tv4",
-            location: location.pathname.replace(/\/[^/]+$/, "") + "plugins/layer_selector/lib",
-            main: "tv4.min"
-        }
-    ]
-});
-
 define([
-        "dojo/_base/declare",
-        "framework/PluginBase",
-        "./LayerManager",
-        "./Ui",
-        "dojo/text!./layers.json",
-        "dojo/text!./templates.html",
-        "jquery"
-    ],
-    function (declare, PluginBase, LayerManager, Ui, layerSourcesJson, templates, $) {
-
-        return declare(PluginBase, {
+    "dojo/_base/declare",
+    "plugins/layer_selector/main",
+    "dojo/text!./layers.json"],
+    function (declare,
+              LayerSelectorPlugin,
+              layerSourcesJson) {
+        return declare(LayerSelectorPlugin, {
             toolbarName: "Flood & Sea Level Rise",
             fullName: "Visualize inundation scenarios from coastal and riverine flooding, storms and sea level rise",
             toolbarType: "sidebar",
             allowIdentifyWhenActive: true,
-			infoGraphic: "plugins/flood_slr/slr_flooding_c.jpg",
+	    infoGraphic: "plugins/flood_slr/slr_flooding_c.jpg",
 
-            _layerManager: null,
-            _ui: null,
-            _currentState: {},
-
-            initialize: function (frameworkParameters) {
-                declare.safeMixin(this, frameworkParameters);
-                this._layerManager = new LayerManager(this.app);
-                this._ui = new Ui(this.container, this.map, templates);
-
-                // Load layer sources, then render UI passing the tree of layer nodes
-                var self = this;
-                this._layerManager.load(layerSourcesJson, function (tree) {
-                    if (self._currentState) {
-                        self._layerManager.setServiceState(self._currentState, self.map);
-                    }
-                    self._ui.render(tree);
-                    $('a.pluginLayerSelector-clear').click(function () { self.clearAll(); });
-                });
-            },
-
-            activate: function () {
-                if (this._currentState) {
-                    this._layerManager.setServiceState(this._currentState, this.map);
-                }
-                this._ui.display();
-            },
-
-            deactivate: function () {
-                this._ui.hideAll();
-                this._currentState = this._layerManager.getServiceState();
-            },
-
-            hibernate: function () {
-                this._currentState = this._layerManager.getServiceState();
-                this._layerManager.hideAllLayers(this.map);
-            },
-
-            getState: function () {
-                return this._layerManager.getServiceState();
-            },
-
-            setState: function (state) {
-                this._currentState = state;
-            },
-
-            clearAll: function () {
-                this._layerManager.hideAllLayers(this.map);
-                this._ui.uncheckAndCollapse();
-                this._currentState = {};
+            getLayersJson: function() {
+                return layerSourcesJson;
             }
-
         });
     }
 );
